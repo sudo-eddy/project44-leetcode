b756416a9eebce87a65121c9e01a85f424846c8b
Eddy Castillo <sudo.coding@protonmail.com>
2019-12-01 05:05:03 -0600

    finished and commented

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -19,3 +19,55 @@ Explanation: There are three ways to climb to the top.
 1. 1 step + 1 step + 1 step
 2. 1 step + 2 steps
 3. 2 steps + 1 step */
+
+class Solution {
+    public int climbStairs(int n) {
+        
+        if (n < 2) {
+            return n;
+        }
+        
+        int fibX = 1; 
+        int fibY = 1;
+        int sum = 0;
+        
+        for (int i = 2; i <= n; i++ ) {
+            System.out.println("This is i --> " + i + " ");
+            System.out.println("This is fibX --> " + fibX + " ");
+            System.out.println("This is fibY --> " + fibY + " ");
+            sum = fibX + fibY;
+            System.out.println("This is sum --> " + sum + " ");
+            fibX = fibY;
+            System.out.println("This is fibX --> " + fibX + " ");
+            fibY = sum;
+            System.out.println("This is new fibY --> " + fibY + " ");
+        }
+        return sum;
+    }
+}
+
+class Solution {
+    public int climbStairs(int n) {
+        
+        if (n < 2) {
+            return n;
+        }
+        
+        int fibX = 1; 
+        int fibY = 1;
+        int sum = 0;
+        
+        for (int i = 2; i <= n; i++ ) {
+            sum = fibX + fibY;
+            fibX = fibY;
+            fibY = sum;
+        }
+        return sum;
+    }
+}
+
+
+/* Thought process
+
+Basically a fibonacci sequence starting at number 2 anything less than two is a base case so we return n
+avoiding recursion by using fibY as a temp variable at the end of each iteration until the loop reaches value of n */
