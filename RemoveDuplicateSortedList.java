ec78b73102360081e8345166d43deb1ddcae9f33
Eddy Castillo <sudo.coding@protonmail.com>
2019-11-30 23:38:34 -0600

    finish & comment

--- RemoveDuplicateSortedList.java
+++ RemoveDuplicateSortedList.java
@@ -8,3 +8,26 @@ Example 2:
 
 Input: 1->1->2->3->3
 Output: 1->2->3 */
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode current = head;
+
+        while (current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
+
+/* Thought process
+
+travese until next node is null
+check if nodes are equal and replace for the next.next node */
